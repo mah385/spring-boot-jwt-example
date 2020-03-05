674124a8db01ed8ff2e67f3480f46e2c6bf12daa
mah385 <santhosh.p@gofrugal.com>
2020-03-05 11:53:13 +0530

    first commit

---
+++ src/main/java/com/mah/springbootjwteg/SpringbootjwtegApplication.java
@@ -0,0 +1,13 @@
+package com.mah.springbootjwteg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjwtegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjwtegApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mah/springbootjwteg/SpringbootjwtegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mah.springbootjwteg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjwtegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
